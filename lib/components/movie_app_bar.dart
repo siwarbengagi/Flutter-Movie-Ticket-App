022dd603e2c164baec37d2315a247dc3e990e236
Siwar <siwarbengagi@gmail.com>
2024-12-03 18:29:49 +0100

    Initial commit for Flutter-Movie-Ticket-App

--- lib/components/movie_app_bar.dart
+++ lib/components/movie_app_bar.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-import 'package:movie_ticket_app/components/search_bar.dart';
 import 'package:movie_ticket_app/const.dart';
 
 class MovieAppBar extends StatelessWidget {
@@ -13,15 +11,13 @@ class MovieAppBar extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
-        // todo: convert this to it's own widget 
+        // todo: convert this to it's own widget
         Container(
           width: MediaQuery.of(context).size.width * .15,
           height: 60.0,
           decoration: kRoundedFadedBorder,
-          child:
-              IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
+          child: IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
         ),
-        const SearchBar(hint: 'Search Movies..'),
       ],
     );
   }
